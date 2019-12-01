219b3ead200d8a02d49c57f9b0070722ca64af1e
YAVUZ KILIÇ <yavuz.kilic@koton.com.tr>
2019-12-02 00:07:23 +0300

    'AdapterPattern'

---
+++ AdapterPattern/AdapterPattern/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace AdapterPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWeb web = null;
+            WebAdapter webAdapter = new WebAdapter(web);
+            webAdapter.Send();
+        }
+
+        interface IEmail
+        {
+            void Send();
+        }
+
+        class Email : IEmail
+        {
+            public void Send()
+            {
+                Console.WriteLine("Sending email.");
+            }
+        }
+
+        public interface IWeb
+        {
+            void Push();
+        }
+
+        public class WebAdapter : IEmail
+        {
+            private IWeb web;
+
+            public WebAdapter(IWeb web)
+            {
+                this.web = web;
+            }
+
+            public void Send()
+            {
+                web.Push();
+            }
+        }
+    }
+}
